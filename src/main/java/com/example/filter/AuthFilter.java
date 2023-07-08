e1837b96d3ce83dec05c2f3440d10448a105e8a6
symbatDeveloper <symbatzinatova.k@gmail.com>
2023-07-08 20:56:06 +0600

    done

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,30 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+@WebFilter(urlPatterns = "/user/*")
+
+    public class AuthFilter implements Filter {
+
+    public void init(FilterConfig config) throws ServletException {
+        // Get init parameter
+        String username = config.getInitParameter("username");
+        String password = config.getInitParameter("password");
+        // Print the init parameter
+        System.out.println("Username : " + username);
+        System.out.println("Password : " + password);
+    }
+@Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+    if (((HttpServletRequest) request).getSession().getAttribute("user") == null) {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
 }
+    }
--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,16 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServlet;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("servletTimeInit", LocalDateTime.now());
+    }
 }
