9b041a07f0bdc864af9c54197c4b191d1f021233
symbatDeveloper <symbatzinatova.k@gmail.com>
2023-07-08 20:59:58 +0600

    in and out fixed

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -1,5 +1,24 @@
 package com.example.servlet;
 
-public class LogoutServlet {
-    //write your code here!
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user") != null) {
+            session.setAttribute("user", null);
+            session.invalidate();
+            req.getRequestDispatcher("login.jsp")
+                    .forward(req, resp);
+        }
+    }
 }
